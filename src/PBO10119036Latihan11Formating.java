04d1a3ff41a12214b20d7fb15cd895ffa929122a
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-10-15 13:32:18 +0700

    Project 11

---
+++ src/PBO10119036Latihan11Formating.java
@@ -0,0 +1,30 @@
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * NAMA     : ALFI NURIZKYA
+ * KELAS    : PBO
+ * NIM      : 10119036
+ * DESKRIPSI PROGRAM : PERINTAH MEMASUKKAN DATA FORMATING
+ */
+public class PBO10119036Latihan11Formating {
+
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -1;
+        System.out.println("i     : " + i);
+        System.out.printf("%%d    : %d%n ", i);
+        System.out.printf("%%10d  : %10d%n ", i);
+        System.out.printf("%%+10d : %+10d%n ", i);
+        System.out.printf("%%+10d : %+10d%n ", iMinus);
+        System.out.printf("%%,10d : %,10d%n ", i);
+        System.out.printf("%%-10d : %-10d%n ", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n ", f);
+        System.out.printf("%%.2f    : %.2f%n ", f);
+        System.out.printf("%%12.2f  : %12.2f%n ", f);
+        System.out.printf("%%,12.2f : %,12.2f%n ", f);
+    }
+    
+}
